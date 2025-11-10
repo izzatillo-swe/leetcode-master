fd7de5e66669fcf9a4ecd7bdb039efdba0c62d6d
izzatillo-swe <izzatillotilavoldiyev5@gmail.com>
2025-11-10 17:45:28 +0500

    twoSum problem has solved

--- src/main/java/leetcode/leetcodemaster/array/ArrayProblems.java
+++ src/main/java/leetcode/leetcodemaster/array/ArrayProblems.java
@@ -1,12 +1,28 @@
 package leetcode.leetcodemaster.array;
 
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArrayProblems {
 
     public static void main(String[] args) {
-
+        int[] ints = twoSum(new int[]{1, 7, 4, 2, 15}, 17);
+        System.out.println(Arrays.toString(ints));
     }
 
-
+    // Two Sum -> Easy
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = target - nums[i];
+            if (map.containsKey(n)) {
+                return new int[]{map.get(n), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
 
 }
