7ed1fe4d4d099e276b93c065eb37dadb91341560
izzatillo-swe <izzatillotilavoldiyev5@gmail.com>
2025-11-11 14:01:26 +0500

    remove element leetcode problem has solved

--- src/main/java/leetcode/leetcodemaster/array/ArrayProblems.java
+++ src/main/java/leetcode/leetcodemaster/array/ArrayProblems.java
@@ -1,15 +1,20 @@
 package leetcode.leetcodemaster.array;
 
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ArrayProblems {
 
     public static void main(String[] args) {
-        int[] ints = twoSum(new int[]{1, 7, 4, 2, 15}, 17);
-        System.out.println(Arrays.toString(ints));
+        // Two Sum -> Easy
+//        int[] ints = twoSum(new int[]{1, 7, 4, 2, 15}, 17);
+//        System.out.println(Arrays.toString(ints));
+
+        // Remove Elements
+//        int i = removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
+//        int i = removeElement(new int[]{3, 2, 2, 3}, 3);
+//        System.out.println(i);
     }
 
     // Two Sum -> Easy
@@ -23,6 +28,18 @@ public class ArrayProblems {
             map.put(nums[i], i);
         }
         return null;
+    }
+
+    // Remove Element -> Easy
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
     }
 
 }
