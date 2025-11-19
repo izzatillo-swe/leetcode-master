8ac7cb954e7bdcd9547c445db5c461da56ab0903
izzatillo-swe <izzatillotilavoldiyev5@gmail.com>
2025-11-19 15:54:43 +0500

    learned 2 problems have solved

---
+++ src/main/java/leetcode/leetcodemaster/test/Test.java
@@ -0,0 +1,28 @@
+package leetcode.leetcodemaster.test;
+
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+//        System.out.println(ts(3)); // 1, 3, 5, 7, 9
+        int[] ncs = ncs(new int[]{1, 2, 0, 0, 3, 4, 0, 5, 6});
+        System.out.println(Arrays.toString(ncs));
+    }
+
+    public static int ts(int n) {
+        if (n % 2 == 0) return (n / 2);
+        else return n / 2 + 1;
+    }
+
+    public static int[] ncs(int[] arr) {
+        int[] res = new int[arr.length];
+        int index = 0;
+        for (int i : arr) {
+            if (i != 0) res[index++] = i;
+        }
+        return res;
+    }
+
+}
