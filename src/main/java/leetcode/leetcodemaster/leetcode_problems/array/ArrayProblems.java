7e25792dbc716f8bcbae58541034d26dd5b812c4
izzatillo-swe <izzatillotilavoldiyev5@gmail.com>
2025-12-10 22:02:14 +0500

    350 - leetcode problem has solved

--- src/main/java/leetcode/leetcodemaster/leetcode_problems/array/ArrayProblems.java
+++ src/main/java/leetcode/leetcodemaster/leetcode_problems/array/ArrayProblems.java
@@ -83,7 +83,11 @@ public class ArrayProblems {
 //        System.out.println(i);
 
         // Intersection of Two Arrays
-        int[] intersection = intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2});
+//        int[] intersection = intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2});
+//        System.out.println(Arrays.toString(intersection));
+
+        // Intersection of Two Arrays ||
+        int[] intersection = intersect2(new int[]{4,9,5}, new int[]{9,4,9,8,4});
         System.out.println(Arrays.toString(intersection));
     }
 
@@ -368,6 +372,7 @@ public class ArrayProblems {
 //        return prefix[right] - prefix[left-1];
 //    }
 
+    // Intersection of Two Arrays
     public static int[] intersection(int[] nums1, int[] nums2) {
         if (nums1.length > nums2.length) {
             return intersection(nums2, nums1);
@@ -381,6 +386,33 @@ public class ArrayProblems {
         Set<Integer> result = new HashSet<>();
         for (int num : nums2) {
             if (set.contains(num)) {
+                result.add(num);
+            }
+        }
+
+        int[] res = new int[result.size()];
+        int i = 0;
+        for (int n : result) {
+            res[i++] = n;
+        }
+
+        return res;
+    }
+
+    // Intersection of Two Arrays ||
+    public static int[] intersect2(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect2(nums2, nums1);
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int num : nums1) {
+            if (map.containsKey(num)) {
                 result.add(num);
             }
         }
